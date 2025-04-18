f8356a99859fe11a4afc73d90bb9e43cb0332f01
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-18 18:09:43 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:d4_8_7/utils/extensions/color_extension.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../utils/extensions/text_style_extention.dart';
 
@@ -33,7 +35,17 @@ class HomePage extends StatelessWidget {
                   ),
                   Spacer(),
                   CupertinoButton(
-                    onPressed: () {},
+                    borderRadius: BorderRadius.circular(100),
+                    padding: EdgeInsets.all(15),
+                    onPressed: () {
+                      Get.closeAllSnackbars();
+                      Get.snackbar(
+                        "Test",
+                        "Test Body",
+                        snackPosition: SnackPosition.BOTTOM,
+                      );
+                    },
+                    color: context.cardColor,
                     child: Icon(Icons.person),
                   ),
                 ],
